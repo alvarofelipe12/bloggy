f3f6909d1299a6e32c2318905e11118884eb679f
Alvaro Felipe Garcia Mendez <alvarofelipe12@gmail.com>
2021-03-16 17:46:46 -0500

    Update Posts.vue
    - Added blog entry.

--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -2,23 +2,37 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Posts</ion-title>
+        <ion-title>BLOGGY 😃</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Posts</ion-title>
-        </ion-toolbar>
+        <ion-toolbar> <ion-title size="large">Posts</ion-title> </ion-toolbar>Î
       </ion-header>
-
-      <ion-list v-if="posts.list && posts.list.length > 0" inset="true" lines="inset">
-        <ion-item v-for="item in posts.list" :key="item.id">
-          <ion-label>
-            <h2>{{ item.title }}</h2>
-            <p>{{ item.body }}</p>
-          </ion-label>
-        </ion-item>
+      <ion-grid>
+        <ion-row>
+          <ion-col class="ion-padding">
+            <h1>{{ title }}</h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+            minima cupiditate non neque debitis harum dolorem distinctio, minus
+            esse blanditiis asperiores ipsum tempore amet ab quibusdam fugiat
+            quaerat, facere aut!
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+      <ion-list
+        v-if="posts.list && posts.list.length > 0"
+        inset="true"
+        lines="inset"
+      >
+        <ion-card v-for="item in posts.list" :key="item.id">
+          <ion-item>
+            <ion-label>
+              <h2>{{ item.title }}</h2>
+              <p>{{ item.body }}</p>
+            </ion-label>
+          </ion-item>
+        </ion-card>
       </ion-list>
       <ion-label v-else>
         {{ posts.error }}
@@ -34,10 +48,17 @@ import {
   IonToolbar,
   IonTitle,
   IonContent,
+  IonList,
+  IonCard,
+  IonItem,
+  IonLabel,
+  IonGrid,
+  IonRow,
+  IonCol,
 } from "@ionic/vue";
-import { ref, reactive } from "vue";
+import { ref, reactive, defineComponent } from "vue";
 
-export default {
+export default defineComponent({
   name: "Posts",
   components: {
     IonHeader,
@@ -45,31 +66,46 @@ export default {
     IonTitle,
     IonContent,
     IonPage,
+    IonList,
+    IonCard,
+    IonItem,
+    IonLabel,
+    IonGrid,
+    IonRow,
+    IonCol,
   },
   data() {
     const posts = reactive({
       list: [],
       error: {},
     });
-    const submitted = async () => {
+    return {
+      posts,
+      title: "Blog Entry",
+    };
+  },
+  methods: {
+    async submitted(): Promise<void> {
       try {
         const response = await fetch(
           `https://jsonplaceholder.typicode.com/posts`
         );
         const json = await response.json();
-        posts.list = ref(json);
-        // console.log(posts.list);
+        this.posts.list = ref(json);
       } catch (error) {
-        // console.log(error);
-        posts.error = reactive({ error });
+        this.posts.error = { error };
       } finally {
-        // do something
+        // do something else
       }
-    };
-    submitted();
-    return {
-      posts,
-    };
+      return;
+    },
   },
-};
+  created() {
+    // fetch("https://jsonplaceholder.typicode.com/posts").then(list => {
+    //   console.log("Component has been created!");
+    //   this.posts.list = list as any;
+    // });
+    this.submitted();
+  },
+});
 </script>
